8ccedc646a70406b9f7c675aa7027b4e3b56fbc8
Sebastian Schulz <16666034+seb-schulz@users.noreply.github.com>
2024-06-13 17:23:33 +0200

    Fix release workflow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,4 +10,18 @@ lint_packages=(lintian shellcheck)
 build_packages=(build-essential devscripts debhelper pandoc fakeroot)
 tests_packages=(mkosi qemu-system-x86 kmod jq systemd-boot cpio zstd systemd-ukify dosfstools mtools file linux-image-amd64)
 
-$SUDO apt-get install --no-install-recommends -y "${lint_packages[@]}" "${build_packages[@]}" "${tests_packages[@]}"
+[[ ${ONLY_BUILD:-} = "1" ]] && $SUDO apt-get install --no-install-recommends -y "${build_packages[@]}"
+[[ -z ${ONLY_BUILD:-} ]] && $SUDO apt-get install --no-install-recommends -y "${lint_packages[@]}" "${build_packages[@]}" "${tests_packages[@]}"
+
+if [[ ${WITH_GH:-} = "1" ]]; then
+    (type -p wget >/dev/null || (apt update && apt-get install wget -y))
+
+    mkdir -p /etc/apt/keyrings
+    wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null
+    chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    apt update
+    apt install gh -y
+fi
+
+exit 0
